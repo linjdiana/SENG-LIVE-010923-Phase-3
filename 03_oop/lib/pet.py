9c88cf8df26da18db7790fe8df5725f97e570074
+ <+>
2023-02-23 16:43:18 -0800

    lecture done

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -6,7 +6,14 @@ import ipdb
 
 # Classes
 
+    # Blueprints => We Create Instances From Blueprint
+    # Cookie Cutter => Instances (Cookies)
+
 # 1. ✅ Create a Pet class
+# class Pet:
+    # pass
+
+# ipdb.set_trace()
 
     # Note: Add 'pass' to the Pet class 
 
@@ -14,20 +21,50 @@ import ipdb
 
     # Compare the Pet instances. Are each of them the same object?
 
+        # No, they are different Objects in Memory but are all created
+        # from the same source (Pet)
+
 # 3. ✅ Demonstrate __init__ 
 
-    # Add arguments to instances  
-    
-    # Use dot notation to access each Pet instance's attributes 
+# self => Instance of Pet Class
 
-    # Update attributes with new values
+# Attributes:
+    # name
+    # age
+    # breed
+    # temperament
+class Pet:
+    def __init__(self, name, age, breed, temperament, owner="No Owner"):
+        
+        # Attached Incoming Arguments to "self" (Instance)
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.temperament = temperament
+        self.owner = owner
+
+# fido = Pet("Fido", 5, "Boxer", "Tranquil")
+# spot = Pet("Spot", 7, "Terrier", "Feisty", "Louis")
+
+# ipdb.set_trace()
+
+    # DONE - Add arguments to instances  
+    
+    # DONE - Use dot notation to access / set each Pet instance's attributes 
+
+    # DONE - Update attributes with new values
 
 # Instance Methods
+
+    # def say_hello(self):
+    #     print("Hello!")
+
+# ipdb.set_trace()
 
 # 4. ✅ Create a "print_pet_details" function that will print each Pet instance's 
 # attributes
 
-    # Review the "self" keyword 
+    # DONE - Review the "self" keyword 
     
     # Invoke "print_pet_details" on an instance 
     
@@ -36,13 +73,40 @@ import ipdb
         # age: 11
         # breed: Domestic Longhair
         # temperament: Sweet
+    
+    def print_pet_details(self):
+        print(f'''
+            name: {self.name}
+            age: {self.age}
+            breed: {self.breed}
+            temperament: {self.temperament}
+        ''')
+
 
 # Object Properties => Attributes that are controlled by methods
 
     # Create an Owner class with two instance methods:
-
+class Owner:
+    def __init__(self, age):
+        self.age = age
         # get_name => Retrieve Owner's name
         
+
+    def get_name(self):
+        print("Retrieving Owner's name...")
+        return self._name
+
+
+    def set_name(self, name):
+        print("Setting Owner's name...")
+
+        if (isinstance(name, str)):
+
+            self._name = name
+
+        else: 
+            print("Name must be a String")
+
         # set_name => Set Owner's name
 
             # Ensure that Owner's name is a String
@@ -51,3 +115,7 @@ import ipdb
 
     # Use property() to compile get_name / set_name and invoke them
     # whenever we access an Owner instance's name
+
+    name = property(get_name, set_name)
+
+ipdb.set_trace()
